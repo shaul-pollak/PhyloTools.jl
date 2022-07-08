eadc094c0e8e51db1baaee6eb2108d04fc34e432
= <=>
2022-07-08 15:16:08 +0000

    update plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,29 +1,29 @@
-function treepositions(tr::N, transform=false; upwards=false) where N
-    o = postwalk(tr);
-    n = length(o);
-    h = getheights(tr);
-    maxh = maximum(values(h));
-    nodepos = Dict{N,Tuple{Float64,Float64}}();
-    i = 0.;
-    for n in o
-        if isleaf(n) 
-            i += 1.;
-            hn = transform ? maxh : h[id(n)];
-        else
-            hn = h[id(n)];
-        end
-        nodepos[n] = upwards ? (i,hn) : (hn, i);
+function treepositions(tr::Node{I,T}, transform=false; upwards=false) where {I,T}
+  o = postwalk(tr);
+  n = length(o);
+  h = getheights(tr);
+  maxh = maximum(values(h));
+  nodepos = Dict{I,Tuple{Float64,Float64}}();
+  i = 0.;
+  for n in o
+    if isleaf(n) 
+      i += 1.;
+      hn = transform ? maxh : h[id(n)];
+    else
+      hn = h[id(n)];
     end
-    function walk(n)
-        isleaf(n) && return nodepos[n]
-        cs = map(walk, children(n));
-        xn = upwards ? nodepos[n][2] : nodepos[n][1];
-        yn = (upwards ? sum(first.(cs)) : sum(last.(cs))) / length(cs);
-        nodepos[n] = upwards ? (yn,xn) : (xn, yn);
-    end
-    walk(tr)
-    # if the root branch has non-zero length we add this:
-    return nodepos
+    nodepos[id(n)] = upwards ? (i,hn) : (hn, i);
+  end
+  function walk(n)
+    isleaf(n) && return nodepos[id(n)]
+    cs = map(walk, children(n));
+    xn = upwards ? nodepos[id(n)][2] : nodepos[id(n)][1];
+    yn = (upwards ? sum(first.(cs)) : sum(last.(cs))) / length(cs);
+    nodepos[id(n)] = upwards ? (yn,xn) : (xn, yn);
+  end
+  walk(tr)
+  # if the root branch has non-zero length we add this:
+  return nodepos
 end
 
 @recipe function f(tree::Node; showlabels=false, internal=true, fs=3, upwards=false, nodes="all")
@@ -42,7 +42,7 @@ end
   for n in toloop
     if isroot(n) 
       if isfinite(distance(n))
-        (x, y) = d[n];
+        (x, y) = d[id(n)];
         @series begin
           seriestype --> :path
           seriescolor --> :black
@@ -53,9 +53,9 @@ end
       @series begin
         seriestype --> :path
         seriescolor --> :black
-        (x1, y1) = d[n]
+        (x1, y1) = d[id(n)]
         if nodes == "all"
-          (x2, y2) = d[parent(n)]
+          (x2, y2) = d[id(parent(n))]
           upwards ? [(x2, y2), (x1, y2), (x1, y1)] : [(x2, y2), (x2, y1), (x1, y1)]
         else
           [(x1,y1)]
@@ -65,7 +65,7 @@ end
   end
   if showlabels==true
     ns = internal ? prewalk(tree) : getleaves(tree)
-    anns = [(d[n]..., (" " * name(n), fs, :left)) for n in ns]
+    anns = [(d[id(n)]..., (" " * name(n), fs, :left)) for n in ns]
     @series begin
       annotations := anns
       [], []
@@ -78,7 +78,6 @@ end
 @recipe function f(tp::TipPoint)
   nodes := "leaves";
   upwards --> false;
-  markerstrokewidth := 0;
   seriestype := :scatter
   tree = tp.args[1];
   l = length(tp.args)
@@ -93,13 +92,11 @@ end
   tree = np.args[1];
   nodes := "nodes";
   upwards --> true;
-  markerstrokewidth := 0;
   seriestype := :scatter
   tree
 end
 
-@recipe function f(tree::Node,colDict::Dict; 
-    showlabels=false, fs=3, upwards=false)
+@recipe function f(tree::Node,colDict::Dict; upwards=false)
   d = treepositions(tree,upwards=upwards)
   framestyle --> :none
   grid --> false
@@ -109,7 +106,7 @@ end
     @series begin
       markercolor := colDict[name(n)]
       markerstrokecolor := colDict[name(n)]
-      (x1, y1) = d[n]
+      (x1, y1) = d[id(n)]
       [(x1,y1)]
     end
   end
