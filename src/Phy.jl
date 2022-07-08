a115610f67ee861b8334d964a9410474c022a5f4
= <=>
2022-07-08 11:28:57 +0000

    Initial commit

--- src/Phy.jl
+++ src/Phy.jl
@@ -1,5 +1,23 @@
 module Phy
 
-# Write your package code here.
+using AbstractTrees
+using RecipesBase
+using Colors
+
+export Node, NewickData
+export isroot, isleaf, postwalk, prewalk, children, sister
+export getroot, getlca, getleaves, nodefilter
+export insertnode!, print_tree, readnw, writenw, @nw_str
+export distance, name, id, nwstr, degree, getheights
+export set_outgroup!, set_outgroup
+export leafnames, Ntip, cophenetic, make_independent_tree, reroot!, midpoint_root
+export tippoint!, nodepoint!, cluster_tree, extract_with_tips
+export treeVI!, duplication_score
+
+include("node.jl")
+include("parser.jl")
+include("functions.jl")
+include("plotting.jl")
+include("treemi.jl")
 
 end
